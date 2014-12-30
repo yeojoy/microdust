847dc1ac40a79366fb83c2b9bba16cb2ffb55286
yeojoy <yeojoy.kim@gmail.com>
2014-12-30 13:50:00 +0900

    versionName, versionCode update

--- app/src/main/java/me/yeojoy/microdustwarning/util/DustUtils.java
+++ app/src/main/java/me/yeojoy/microdustwarning/util/DustUtils.java
@@ -87,7 +87,8 @@ public class DustUtils implements DustConstants {
         Intent intent = new Intent(context, me.yeojoy.microdustwarning.DustActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
         mBuilder.setContentIntent(pendingIntent);
 
         // Add Action Buttons to Wear
