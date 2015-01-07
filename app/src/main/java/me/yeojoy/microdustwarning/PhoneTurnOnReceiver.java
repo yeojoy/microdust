a7dd500fafb0f4d6e4060e8c54310f7036cdec97
yeojoy <yeojoy.kim@gmail.com>
2015-01-07 16:59:38 +0900

    다시 추가

---
+++ app/src/main/java/me/yeojoy/microdustwarning/PhoneTurnOnReceiver.java
@@ -0,0 +1,21 @@
+package me.yeojoy.microdustwarning;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import me.yeojoy.microdustwarning.alarm.AlarmHelper;
+
+/**
+ * Created by yeojoy on 15. 1. 7..
+ */
+public class PhoneTurnOnReceiver extends BroadcastReceiver {
+    private static final String TAG = PhoneTurnOnReceiver.class.getSimpleName();
+    
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive()");
+        AlarmHelper.launchAlarmManager(context.getApplicationContext());
+    }
+}
